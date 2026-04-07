778898c639769f289901a4e925137f4c64aff109
trcn_a <turcinalina279@gmail.com>
2026-04-07 16:48:47 +0300

    feat: add task priority styling

--- kanban-project/src/components/Task.tsx
+++ kanban-project/src/components/Task.tsx
@@ -5,10 +5,19 @@ type Props = {
     task: Task;
 }
 export default function Task({task}: Props) {
+
+    const priorityClassMap = {
+        1: styles.one,
+        2: styles.two,
+        3: styles.three,
+        4: styles.four,
+        5: styles.five,
+    };
+
     return (
         <>
             <div className={styles.task}>
-                <p className={styles.priority}> {task.priority} </p>
+                <p className={` ${styles.priority} ${priorityClassMap[task.priority as 1 | 2 | 3 | 4 | 5]}`}> {task.priority} </p>
                 <h3 className={styles.title}> {task.title} </h3>
             </div>
         </>
